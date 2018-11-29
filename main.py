5ff6115870c5b5425618fabf3f95016b3df24400
Bryan-Yi <LJY571227068@outlook.com>
2018-11-29 22:45:08 +0800

    delete bullet out of range

--- main.py
+++ main.py
@@ -49,6 +49,39 @@ class HeroPlane(object):
         self.bullet_list.append(Bullet(self.screen, self.x, self.y))
 
 
+class EnemyPlane(object):
+    def __init__(self, screen_temp):
+        self.x = 0
+        self.y = 0
+        self.screen = screen_temp
+        self.image = pygame.image.load("./images/enemy0.png")
+        # 存储发射出去的子弹
+        self.bullet_list = []
+        self.direction = "right"
+
+    def display(self):
+        self.screen.blit(self.image, (self.x, self.y))
+        for bullet in self.bullet_list:
+            bullet.display()
+            bullet.move()
+            if bullet.judge():
+                self.bullet_list.remove(bullet)
+
+
+    def move(self):
+        if self.direction == "right":
+            self.x += 5
+        elif self.direction == "left":
+            self.x -= 5
+        if self.x > 430:
+            self.direction = "left"
+        elif self.x < 0:
+            self.direction = "right"
+
+    def fire(self):
+        self.bullet_list.append(Bullet(self.screen, self.x, self.y))
+
+
 def key_control(hero_temp):
     # 获取用户点击事件
     for event in pygame.event.get():
@@ -90,6 +123,12 @@ class Bullet(object):
     def move(self):
         self.y -= 5
 
+    def judge(self):
+        if self.y < 0:
+            return True
+        else:
+            return False
+
 
 def main():
     # 1、创建窗口
@@ -98,12 +137,14 @@ def main():
     background = pygame.image.load("./images/background.png")
     # 3、创建一个飞机
     hero = HeroPlane(screen)
-    # 4、创建子弹
-    # bullet = Bullet(screen)
+    # 4、创建敌机
+    enemy = EnemyPlane(screen)
 
     while True:
         screen.blit(background, (0, 0))
         hero.display()
+        enemy.display()
+        enemy.move()
         pygame.display.update()
         key_control(hero)
         time.sleep(0.01)
